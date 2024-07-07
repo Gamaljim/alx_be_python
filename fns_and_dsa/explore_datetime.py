cc16a6a093e4e1f8c9c3f1bb092916a3a76e03a4
Gamal Ali <gamal.adbelazez94@gmail.com>
2024-07-07 15:18:42 +0400

    3rd task

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -3,7 +3,8 @@ from datetime import datetime, timedelta
 
 def display_current_datetime():
     current_date = datetime.now()
-    return current_date
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    return formatted_date
 
 
 def calculate_future_date():
