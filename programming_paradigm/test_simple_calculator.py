ecda997f8160098e35540f9eca87bfe31a769f9d
Gamal Ali <gamal.adbelazez94@gmail.com>
2024-07-12 01:01:11 +0400

    adding oop task3

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,23 +4,23 @@ from simple_calculator import SimpleCalculator
 
 class TestCal(unittest.TestCase):
     def setUp(self):
-        self.cal = SimpleCalculator()
+        self.calc = SimpleCalculator()
 
     def test_addition(self):
-        self.assertEqual(self.cal.add(5, 5), 10)
-        self.assertEqual(self.cal.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(5, 5), 10)
+        self.assertEqual(self.calc.add(-1, 1), 0)
 
     def test_subtract(self):
-        self.assertEqual(self.cal.subtract(5, 5), 0)
-        self.assertEqual(self.cal.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(5, 5), 0)
+        self.assertEqual(self.calc.subtract(10, 5), 5)
 
     def test_multiply(self):
-        self.assertEqual(self.cal.multiply(5, 5), 25)
-        self.assertEqual(self.cal.multiply(10, 5), 50)
+        self.assertEqual(self.calc.multiply(5, 5), 25)
+        self.assertEqual(self.calc.multiply(10, 5), 50)
 
     def test_divide(self):
-        self.assertEqual(self.cal.divide(10, 5), 2)
-        self.assertIsNone(self.cal.divide(10, 0))
+        self.assertEqual(self.calc.divide(10, 5), 2)
+        self.assertIsNone(self.calc.divide(10, 0))
 
 
 if __name__ == '__main__':
